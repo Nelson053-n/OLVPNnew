be6f1dde83b844cb4b15ea5a76fc7f679878507c
Nelson053 <aksys.nel@gmail.com>
2025-12-17 02:13:02 +0000

    Обновлены клавиатуры: удалена кнопка "Инструкция" из стартового меню и добавлена загрузка цен из JSON для выбора срока подписки с отображением цен.

--- core/keyboards/start_button.py
+++ core/keyboards/start_button.py
@@ -8,10 +8,8 @@ def start_keyboard() -> InlineKeyboardMarkup:
 
     :return: InlineKeyboardMarkup - Объект InlineKeyboardMarkup, содержащий клавиатуру.
     """
-    link_instruction = f"https://telegra.ph/Instrukciya-k-OLVPN-03-13-2"
     keyboard_builder = InlineKeyboardBuilder()
     keyboard_builder.button(text='🔑 Мой ключ', callback_data='my_key')
     keyboard_builder.button(text='💳 Купить ключ', callback_data='get_key')
-    keyboard_builder.button(text='📚 Инструкция', url=link_instruction)
     keyboard_builder.adjust(2)
     return keyboard_builder.as_markup()
